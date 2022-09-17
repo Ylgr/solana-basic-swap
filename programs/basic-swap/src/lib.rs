6d0080887d2ee11d8748f6b87564c401f8c9dd7c
viet tu <quyen.luminous@gmail.com>
2022-09-18 02:25:17 +0700

    (+) create pool

--- programs/basic-swap/src/lib.rs
+++ programs/basic-swap/src/lib.rs
@@ -1,11 +1,26 @@
 use anchor_lang::prelude::*;
+use anchor_spl::token::{self, SetAuthority, Token, TokenAccount, Transfer};
+use spl_token::instruction::AuthorityType;
+
 declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
+
 #[program]
 pub mod basic_swap {
     use super::*;
+
+    const SWAP_PDA_SEED: &[u8] = b"swap";
+
     pub fn create_pool(ctx: Context<CreatePool>, token: Pubkey) -> Result<()> {
         let pool = &mut ctx.accounts.pool;
         pool.token = token;
+        pool.rate = 10;
+
+        let (pda, _bump_seed) = Pubkey::find_program_address(&[SWAP_PDA_SEED], ctx.program_id);
+        token::set_authority(ctx.accounts.into_set_authority_context(), AuthorityType::AccountOwner, Some(pda))?;
+        Ok(())
+    }
+
+    pub fn swap(ctx: Context<Swap>, amount:u64) -> Result<()> {
         Ok(())
     }
 }
@@ -16,16 +31,42 @@ pub struct CreatePool<'info> {
     #[account(
         init,
         payer=signer,
-        space= 8 + 32
+        space= 8 + 32 + 1
     )]
-    pub pool: Pool,
+    pub pool: Account<'info,Pool>,
+    #[account(mut)]
+    pub pda_deposit_token_account: Account<'info, TokenAccount>,
+    pub pda_account: AccountInfo<'info>,
     #[account(mut)]
     pub signer: Signer<'info>,
-    pub system_program: Program<'info,System>
+    pub system_program: Program<'info,System>,
+    pub token_program: Program<'info, Token>
+}
+
+impl<'info> CreatePool<'info> {
+    fn into_set_authority_context(&self) -> CpiContext<'_, '_, '_, 'info, SetAuthority<'info>> {
+        let cpi_accounts = SetAuthority {
+            account_or_mint: self.pda_deposit_token_account.to_account_info().clone(),
+            current_authority: self.pda_account.clone(),
+        };
+        let cpi_program = self.token_program.to_account_info();
+        CpiContext::new(cpi_program, cpi_accounts)
+    }
+}
+
+#[derive(Accounts)]
+#[instruction(amount:u64)]
+pub struct Swap<'info> {
+    #[account(mut)]
+    pub pool: Account<'info,Pool>,
+    #[account(mut)]
+    pub signer: Signer<'info>,
+    pub system_program: Program<'info,System>,
+    pub token_program: Program<'info, Token>
 }
 
 #[account]
 pub struct Pool {
     pub token: Pubkey,
+    pub rate: u8,
 }
-
