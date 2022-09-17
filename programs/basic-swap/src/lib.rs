bfd9b94609588a1627ff52ac112a709330c343c6
viet tu <quyen.luminous@gmail.com>
2022-09-17 23:44:22 +0700

    (tmp) create swap pool

--- programs/basic-swap/src/lib.rs
+++ programs/basic-swap/src/lib.rs
@@ -1,15 +1,31 @@
 use anchor_lang::prelude::*;
-
 declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
-
 #[program]
 pub mod basic_swap {
     use super::*;
-
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+    pub fn create_pool(ctx: Context<CreatePool>, token: Pubkey) -> Result<()> {
+        let pool = &mut ctx.accounts.pool;
+        pool.token = token;
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+#[instruction(token:Pubkey)]
+pub struct CreatePool<'info> {
+    #[account(
+        init,
+        payer=signer,
+        space= 8 + 32
+    )]
+    pub pool: Pool,
+    #[account(mut)]
+    pub signer: Signer<'info>,
+    pub system_program: Program<'info,System>
+}
+
+#[account]
+pub struct Pool {
+    pub token: Pubkey,
+}
+
